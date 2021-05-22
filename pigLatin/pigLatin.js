ed9f19f8d95e34ffa67ba57f213b6422f7d00c1c
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:52:30 -0700

    Complete pigLatin

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,5 +1,31 @@
-function pigLatin(string) {
+const vowels = 'aeiou'
 
-};
-  
-  module.exports = pigLatin;
+function isVowel(letter) {
+    return vowels.includes(letter)
+}
+
+function specialCase(word) {
+    return word[0] !== 'u' && word[word.length - 1] !== 'q'
+}
+
+function pigLatin(phrase) {
+    const words = phrase.split(' ')
+    return words.map(w => {
+        let word = w
+        if (!isVowel(word[0])) {
+            let exit = false
+            while (!exit) {
+                const letters = word.split('')
+                letters.push(letters.shift())
+                word = letters.join('')
+                if (isVowel(word[0]) && specialCase(word)) {
+                    exit = true
+                }
+            }
+        }
+        return word + 'ay'
+    }).join(' ')
+}
+
+
+module.exports = pigLatin
