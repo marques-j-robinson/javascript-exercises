f5ac576e1d4d67daefb7b796b9b2ed413547b8e0
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:55:06 -0700

    Complete snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,17 @@
-const snakeCase = function() {
+function isLower(letter) {
+    return letter.match(/([a-z])/)
+}
 
-};
+const snakeCase = function (phrase) {
+    if (isLower(phrase[0])) {
+        phrase = phrase.replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+    }
+    const re = /([\w]*)/gi
+    const match = phrase.match(re)
+    return match
+        .filter(i => i)
+        .map(i => i.toLowerCase())
+        .join('_')
+}
 
-module.exports = snakeCase;
+module.exports = snakeCase
