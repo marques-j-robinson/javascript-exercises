814b4769105138fd3b5b222c294f9c24e21ff6b0
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:47:21 -0700

    Complete palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 
-};
+function serialize(word) {
+    return word
+        .toLowerCase()
+        .replace(/\s/g, '')
+        .split('')
+        .filter(letter => {
+            return punctuation.indexOf(letter) === -1
+        })
+        .join('')
+}
 
-module.exports = palindromes;
+function flip(word) {
+    const split = word.split('')
+    const revArr = split.reverse()
+    const join = revArr.join('')
+    return join
+}
+
+const palindromes = function (word) {
+    const serializeWord = serialize(word)
+    return serializeWord === flip(serializeWord)
+}
+
+module.exports = palindromes
