ad7dd9b87b14041ca117eb8baa4aa4975322fd7e
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:43:03 -0700

    Complete findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const now = new Date()
+const curyear = now.getFullYear()
+function getAge({yearOfBirth, yearOfDeath = curyear}) {
+    return yearOfDeath - yearOfBirth
+}
 
-};
+let findTheOldest = function (people) {
+    const res = people.map(p => {
+        p.age = getAge(p)
+        return p
+    })
+    res.sort((a, b) => b.age - a.age)
+    return res[0]
+}
 
-module.exports = findTheOldest;
+module.exports = findTheOldest
