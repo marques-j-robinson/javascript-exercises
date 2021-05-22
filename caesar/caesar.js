f3d84d099ef74ca62572d80818cec069e30be5db
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:33:33 -0700

    Complete caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,22 @@
-const caesar = function() {
-
+const caesar = function (input, shiftFactor) {
+    return input
+        .split("")
+        .map(char => shiftChar(char, shiftFactor))
+        .join("");
 };
 
-module.exports = caesar;
+const codeSet = code => (code < 97 ? 65 : 97);
+const mod = (n, m) => (n % m + m) % m;
+
+const shiftChar = (char, shiftFactor) => {
+    const code = char.charCodeAt();
+
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        return String.fromCharCode(
+            mod(code + shiftFactor - codeSet(code), 26) + codeSet(code)
+        )
+    }
+    return char;
+}
+
+module.exports = caesar
