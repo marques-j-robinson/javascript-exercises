6f046edd52f31b5fd397e5d9c6e583f7ac272c55
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 19:51:28 -0700

    Complete reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+    res = ''
+    for (let i = input.length - 1; i >= 0; i--) {
+        res = res + input[i]
+    }
+    return res
 };
 
 module.exports = reverseString;
