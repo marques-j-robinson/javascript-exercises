8d642b430acf89308156c7595828ae4cf552d3cc
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:41:51 -0700

    Complete fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+function fib(num) {
+    if (num <= 1) return 1
+    return fib(num - 1) + fib(num - 2)
+}
 
-};
+const fibonacci = function (num) {
+    if (num < 0) {
+        return 'OOPS'
+    }
+    return fib(num - 1) // subtract 1 due to finding the sequence and that being 0 based
+}
 
 module.exports = fibonacci;
