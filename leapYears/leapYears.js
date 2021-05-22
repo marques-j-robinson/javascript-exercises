a05b5a4d309ac66896c37dc84ae9277382370cfa
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:46:12 -0700

    Complete leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const checkDivisibleBy = function (year, int) {
+    return year % int === 0
+}
 
-};
+const leapYears = function (year) {
+    if (!checkDivisibleBy(year, 4)) {
+        return false
+    }
+    if (checkDivisibleBy(year, 100) && !checkDivisibleBy(year, 400)) {
+        return false
+    }
+    return true
+}
 
-module.exports = leapYears;
+module.exports = leapYears
