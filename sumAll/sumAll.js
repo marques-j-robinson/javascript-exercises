7ce82eccd36a86eab64fcde5e09f5de08d529cc2
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:55:54 -0700

    Complete sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const isPositive = function (val) {
+    return val >= 0
+}
 
-};
+const isNumber = function (val) {
+    return typeof val === 'number'
+}
 
-module.exports = sumAll;
+const sumAll = function (a, b) {
+    // Error Handling
+    if (!isPositive(a) || !isPositive(b) || !isNumber(a) || !isNumber(b)) {
+        return 'ERROR'
+    }
+    // Prep Nums
+    const min = Math.min(a, b)
+    const max = Math.max(a, b)
+    // Iterate and Add
+    let sum = 0
+    let i = min
+    while (i <= max) {
+        sum += i
+        i++
+    }
+    return sum
+}
+
+module.exports = sumAll
