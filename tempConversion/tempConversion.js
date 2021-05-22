55b79360bbddb2b9b449ebae7fdbe0f8074fa7c5
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:57:13 -0700

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,16 @@
-const ftoc = function() {
+const round = function (val) {
+    return Math.round(val * 10) / 10
+}
 
-};
+const ftoc = function (degF) {
+    return round((degF - 32) * 5 / 9)
+}
 
-const ctof = function() {
-
-};
+const ctof = function (degC) {
+    return round(degC * 9 / 5 + 32)
+}
 
 module.exports = {
-  ftoc,
-  ctof
-};
+    ftoc,
+    ctof
+}
