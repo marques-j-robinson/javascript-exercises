2ea0dd3a12c03b0cc28187956ca87bce9dc5bc25
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:40:14 -0700

    Complete calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    return arr.reduce((acc, i) => acc + i, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+    return arr.reduce((acc, i) => acc * i, 1)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    let total = 1
+    while (b > 0) {
+        total *= a
+        b--
+    }
+    return total
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    if (num < 0) {
+        return -1
+    } else if (num === 0) {
+        return 1
+    } else {
+        return (num * factorial(num - 1))
+    }
 };
 
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
