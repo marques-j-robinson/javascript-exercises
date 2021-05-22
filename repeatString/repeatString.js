5a25652019c1c64314416da154ad352e74a5abfc
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 19:47:04 -0700

    Complete repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (input, loopMax) {
+    if (loopMax < 0) {
+        return 'ERROR'
+    }
+    let res = ''
+    let count = 0
+    while (count < loopMax) {
+        res = res + input
+        ++count
+    }
+    return res
 };
 
 module.exports = repeatString;
