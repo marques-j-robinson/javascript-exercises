0b7acc48adceb62cc803b5fd69048faec33cf007
marques-j-robinson <marques.j.robinson@gmail.com>
2021-05-21 20:53:32 -0700

    Complete removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    const list = arguments[0]
+    const res = list.filter(i => {
+        let shouldAdd = true
+        for (let j = 1; j <= arguments.length; j++) {
+            if (i === arguments[j]) {
+                shouldAdd = false
+            }
+        }
+        return shouldAdd
+    })
+    return res
+}
 
-};
-
-module.exports = removeFromArray;
+module.exports = removeFromArray
